575fe58323d4f1efadd8053e64eb037355f5b1cd
Aishwarya Sharma <roli.mradul@gmail.com>
2024-07-06 19:13:27 +0530

    other review points - corrected

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 */
 
 const inputNum = document.getElementById("input_num");
-
 const convertBtn = document.getElementById("convert_btn");
 const meterP = document.getElementById("meter_feet_p");
 const literP = document.getElementById("liter_gallon_p");
@@ -15,19 +14,15 @@ convertBtn.addEventListener("click", function () {
   const input = inputNum.value;
 
   meterP.textContent = `
-                            ${input} meters = ${(input * 3.281).toFixed(
-    3
-  )} feet | 
-                            ${input} feet = ${(input * 0.3048).toFixed(
-    3
-  )} meters
-                        `;
-  literP.textContent = `${input} liters = ${(input * 0.264).toFixed(
-    3
-  )} gallons | 
-  ${input} gallons = ${(input * 3.785412).toFixed(3)} liters
+    ${input} meters = ${(input * 3.281).toFixed(3)} feet | 
+    ${input} feet = ${(input * 0.3048).toFixed(3)} meters
   `;
-  kilogramsP.textContent = `${input} kilograms = ${(input * 2.204).toFixed(
-    3
-  )} pounds | ${input} pounds = ${(input * 0.454).toFixed(3)} kilograms`;
+  literP.textContent = `
+    ${input} liters = ${(input * 0.264).toFixed(3)} gallons | 
+    ${input} gallons = ${(input * 3.785412).toFixed(3)} liters
+  `;
+  kilogramsP.textContent = `
+    ${input} kilograms = ${(input * 2.204).toFixed(3)} pounds | 
+    ${input} pounds = ${(input * 0.454).toFixed(3)} kilograms
+  `;
 });
